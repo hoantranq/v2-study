9f3877e6bfba5cd06eb70bbf1eeebf57459fdd4b
Tran Quoc Hoan <dev.tranquochoan@gmail.com>
2021-11-18 08:58:00 +0700

    Add dashboard. Main page. Etc...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,39 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutComponent } from './modules/core/components/layout/layout.component';
+
+// Ng-Zorro
+import { NgZorroAntdModule } from './modules/core/utils/ng-zorro-antd.module';
+import { LoginComponent } from './modules/features/login/login.component';
+import { RegisterComponent } from './modules/features/register/register.component';
+import { ContentComponent } from './modules/core/components/content/content.component';
+import { MainPageComponent } from './modules/features/main-page/main-page.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+    RegisterComponent,
+    ContentComponent,
+    MainPageComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    // Ng-Zorro-modules
+    NgZorroAntdModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
